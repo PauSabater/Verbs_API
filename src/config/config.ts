4aca1d028584c47aa0d2c246b1837d5142ac0016
PauSabater <pau.sabater.vilar@gmail.com>
2024-01-29 17:59:12 +0100

    Add env vars

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,17 +2,17 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@verbs.xg7zoxw.mongodb.net/`
+const NEXT_MONGO_USERNAME = process.env.NEXT_MONGO_USERNAME
+const NEXT_MONGO_PASSWORD = process.env.NEXT_MONGO_PASSWORD
+const MONGO_URL = `mongodb+srv://${NEXT_MONGO_USERNAME}:${NEXT_MONGO_PASSWORD}@verbs.xg7zoxw.mongodb.net/`
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337
+const NEXT_SERVER_PORT = process.env.NEXT_SERVER_PORT ? Number(process.env.NEXT_SERVER_PORT) : 1337
 
 export const config = {
     mongo: {
         url: MONGO_URL
     },
     server: {
-        port: SERVER_PORT
+        port: NEXT_SERVER_PORT
     }
 }
